38b83b03a0ed8034125577eeac89806adc6552c9
Hyunggeun Ahn <hahn@c1r9s7.42seoul.kr>
2022-06-16 06:02:09 +0900

    save

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "so_long.h"
 
 char	*read_fd(char *remain, int fd)
 {
